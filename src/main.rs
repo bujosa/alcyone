5e6d3b292139b728b30e2e7c05751a315bfe0780
bujosa <davidbujosa@gmail.com>
2023-03-05 03:21:57 -0400

    feat: create a simple request api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Look up the price of a cryptocurrency: ");
+    let mut coin = String::new();
+
+    let _ = std::io::stdin()
+        .read_line(&mut coin)
+        .expect("An error occurred while reading stdin");
+
+    let res = get_price(&coin);
+
+    match res {
+        Ok(price) => println!("The price is: {price}"),
+        Err(error) => println!("Error fetching the coin {error}"),
+    }
+}
+
+fn get_price(coin: &str) -> Result<String, ureq::Error> {
+    let body: String = ureq::get(&format!("https://api.coingecko.com/api/v3/coins/{coin}"))
+        .call()?
+        .into_string()?;
+
+    let parsed: Value = serde_json::from_str(&body).unwrap();
+    Ok(parsed["market_data"]["current_price"]["usd"].to_string())
 }
